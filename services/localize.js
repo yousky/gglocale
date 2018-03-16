775d5b4909a37eefa90a1bd3da5898461e04245a
yousky <y1o2u3@gmail.com>
2018-03-16 15:27:26 +0900

    json file structure change(array -> object)

--- services/localize.js
+++ services/localize.js
@@ -17,7 +17,7 @@ let createXml = function (rows, keyCol, localeCol) {
     let row = rows[index];
     let keyValue = row[keyCol];
     let localeValue = row[localeCol];
-    if(!keyValue){
+    if (!keyValue) {
       console.warn('keyValue null: index=' + index);
       continue;
     }
@@ -33,30 +33,30 @@ let createXml = function (rows, keyCol, localeCol) {
 
 let createXml4resxAsync = function (rows, keyCol, localeCol) {
   return new Promise(function (resolve, reject) {
-    fs.readFile(path.join(workDirectory, 'Language.empty.resx'), 'utf8', function(err, xmlbase){
-      if (err){
-        return reject('readFile Error: '+err);
+    fs.readFile(path.join(workDirectory, 'Language.empty.resx'), 'utf8', function (err, xmlbase) {
+      if (err) {
+        return reject('readFile Error: ' + err);
       }
       let parser = new DOMParser();
       let xmlDoc = parser.parseFromString(xmlbase, "text/xml");
       let strEmpty = serializer.serializeToString(xmlDoc);
       let elem = xmlDoc.getElementsByTagName("root")[0];
-    
+
       for (let index = 0; index < rows.length; index++) {
         let row = rows[index];
         // The key value of resx does not allow special characters such as spaces.
         let keyValue = row[keyCol];
-        if(!keyValue){
+        if (!keyValue) {
           console.warn('keyValue null: index=' + index);
           continue;
         }
-        
+
         let localeValue = row[localeCol];
-        if(localeValue){
+        if (localeValue) {
           // The line break character seems to be google /n. Convert to /r /n for windows C#
-          localeValue = localeValue.replace(/\n/g,'\r\n');
+          localeValue = localeValue.replace(/\n/g, '\r\n');
         }
-    
+
         let nodeName = xmlDoc.createElement("data");
         nodeName.setAttribute("name", keyValue);
         nodeName.setAttribute("xml:space", "preserve");
@@ -65,7 +65,7 @@ let createXml4resxAsync = function (rows, keyCol, localeCol) {
         nodeName.appendChild(nodevalue);
         elem.appendChild(nodeName);
       }
-    
+
       let str = serializer.serializeToString(xmlDoc);
       return resolve(str);
     })
@@ -74,22 +74,20 @@ let createXml4resxAsync = function (rows, keyCol, localeCol) {
 
 let createJson = function (rows, keyCol, localeCol) {
 
-    let objs = [];
-    for (let index = 0; index < rows.length; index++) {
-      let row = rows[index];
-      let keyValue = row[keyCol];
-      let localeValue = row[localeCol];
-      if(!keyValue){
-        console.warn('keyValue null: index=' + index);
-        continue;
-      }
-
-      let obj = {};
-      obj[keyValue] = localeValue;
-      objs.push(obj);
+  let obj = {};
+  for (let index = 0; index < rows.length; index++) {
+    let row = rows[index];
+    let keyValue = row[keyCol];
+    let localeValue = row[localeCol];
+    if (!keyValue) {
+      console.warn('keyValue null: index=' + index);
+      continue;
     }
 
-  return JSON.stringify(objs);
+    obj[keyValue] = localeValue;
+  }
+
+  return JSON.stringify(obj);
 }
 
 let saveAsync = function (docKey, authClientEmail, authPrivateKey, sheetName, keyCol, localeCol, fileType) {
@@ -104,14 +102,14 @@ let saveAsync = function (docKey, authClientEmail, authPrivateKey, sheetName, ke
         }
       }
       let executeCreateFileStrAsync = function (err) {
-        if( err ) {
-          return reject({code : 500, message : 'Google Auth Error: ' + err});
+        if (err) {
+          return reject({ code: 500, message: 'Google Auth Error: ' + err });
         }
         doc.getInfo(function (err, info) {
-          if( err ) {
-            return reject({code : 500, message : 'Google Read[getInfo] Error: ' + err});
+          if (err) {
+            return reject({ code: 500, message: 'Google Read[getInfo] Error: ' + err });
           }
-          console.log('Loaded doc: '+info.title+' by '+info.author.email);
+          console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
           let sheet = null;
           for (let index = 0; index < info.worksheets.length; index++) {
             let sh = info.worksheets[index];
@@ -121,30 +119,30 @@ let saveAsync = function (docKey, authClientEmail, authPrivateKey, sheetName, ke
             }
           }
           if (sheet === null) {
-            return reject({code : 400, message : 'Not found sheet : ' + sheetName});
+            return reject({ code: 400, message: 'Not found sheet : ' + sheetName });
           }
-          console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+          console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
 
           sheet.getRows({
             offset: 1
-          }, function( err, rows ){
-            if( err ) {
-              return reject({code : 500, message : 'Google Read[getRows] Error: ' + err});
+          }, function (err, rows) {
+            if (err) {
+              return reject({ code: 500, message: 'Google Read[getRows] Error: ' + err });
             }
-            console.log('Read '+rows.length+' rows');
-      
+            console.log('Read ' + rows.length + ' rows');
+
             let str = null;
             if (fileType == 'json') {
               str = createJson(rows, keyCol, localeCol);
               resolve(str);
             } else if (fileType == 'xml4android') {
-              str = createXml(rows, keyCol, localeCol);  
+              str = createXml(rows, keyCol, localeCol);
               resolve(str);
             } else {  //resx
               return createXml4resxAsync(rows, keyCol, localeCol).then(function (str) {
                 resolve(str);
               }).catch(function (err) {
-                return reject({code : 500, message : 'createXml4resxAsync Error: ' + err});
+                return reject({ code: 500, message: 'createXml4resxAsync Error: ' + err });
               });
             }
             return
@@ -156,9 +154,9 @@ let saveAsync = function (docKey, authClientEmail, authPrivateKey, sheetName, ke
       } else {
         return executeCreateFileStrAsync();
       }
-      
+
     } catch (err) {
-      return reject({code : 500, message : 'saveAsync Error: ' + err});
+      return reject({ code: 500, message: 'saveAsync Error: ' + err });
     }
   });
 }
